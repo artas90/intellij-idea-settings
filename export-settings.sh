fc9be7709a68d0ee22c39984d3c1d69d56624a26
Andrew Sokolov <artas90@gmail.com>
2024-10-23 13:20:18 +0300

    update keymaps

--- export-settings.sh
+++ export-settings.sh
@@ -1,2 +1,4 @@
 #!/usr/bin/bash
-cp -rf "$HOME/Library/Application Support/JetBrains/IdeaIC2024.1/options" "./settings"
+settings_dir="$HOME/Library/Application Support/JetBrains/IdeaIC2024.1"
+cp -rf "${settings_dir}/options" "./settings"
+cp -rf "${settings_dir}/keymaps" "./settings"
