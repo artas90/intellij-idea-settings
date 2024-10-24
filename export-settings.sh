75dc4092e772eacff57661dd747853d7dd3824b4
Andrew Sokolov <artas90@gmail.com>
2024-10-25 02:00:41 +0300

    improve export-settings script

--- export-settings.sh
+++ export-settings.sh
@@ -1,4 +1,16 @@
 #!/usr/bin/bash
-settings_dir="$HOME/Library/Application Support/JetBrains/IdeaIC2024.1"
+
+if [[ $OSTYPE == darwin* ]]; then
+  jetbrains_dir="$HOME/Library/Application Support/JetBrains"
+elif [[ $OSTYPE == linux-* ]]; then
+  jetbrains_dir="$HOME/.config/JetBrains"
+else
+  echo "only osx or linux is supported"
+  exit
+fi
+
+ide_version=`ls "${jetbrains_dir}" | grep "IdeaIC" | grep -v backup | tail -n1`
+settings_dir="${jetbrains_dir}/${ide_version}"
+
 cp -rf "${settings_dir}/options" "./settings"
 cp -rf "${settings_dir}/keymaps" "./settings"
